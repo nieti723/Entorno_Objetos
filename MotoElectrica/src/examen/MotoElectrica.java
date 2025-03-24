4f59f15d41eaa6d4ac88ae192756e12f553c4782
nieti723 <juan.nieto-acosta@iesruizgijon.com>
2025-03-24 13:08:26 +0100

    Corregido test

---
+++ MotoElectrica/src/examen/MotoElectrica.java
@@ -0,0 +1,121 @@
+/**
+ *
+ * @author 
+ */
+package examen;
+
+public class MotoElectrica {
+
+  //atributos
+  private String marca;
+  private String modelo;
+  private String id;
+  private double velocidad;
+  private int direccion;
+
+  //constructores
+  public MotoElectrica(String marca, String modelo, String id, double velocidad, int direccion) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.id = id;
+    this.velocidad = velocidad;
+    this.direccion = direccion;
+  }
+
+  public MotoElectrica() {
+  }
+
+  //Getters and Setters
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public double getVelocidad() {
+    return velocidad;
+  }
+
+  public void setVelocidad(double velocidad) {
+    this.velocidad = velocidad;
+  }
+
+  public int getDireccion() {
+    return direccion;
+  }
+
+  public void setDireccion(int direccion) {
+    this.direccion = direccion;
+  }
+
+  //m�todos
+  /**
+   * Modifica la velocidad de la moto de forma que cada segundo aumenta en 2
+   * Km/h
+   *
+   * @param segundos double que indica el n�mero de segundos
+   */
+  public void acelerar(double segundos) {
+    double aceleracion = segundos * 2;
+
+    this.velocidad += aceleracion;
+
+    if (this.velocidad > 50) {
+      this.velocidad = 50;
+    }
+  }
+
+  /**
+   * Modifica la velocidad de la moto disminuy�ndola 5 Km/h por cada segundo
+   *
+   * @param segundos double que indica el n�mero de segundos que se pisa el
+   * freno
+   */
+  public void frenar(double segundos) {
+    double freno = segundos * 5;
+
+    this.velocidad -= freno;
+
+    if (this.velocidad < 0) {
+      this.velocidad = 0;
+    }
+
+  }
+
+  /**
+   * Modifica la direcci�n a izquierda o derecha dependiendo del par�metro
+   * introducido y del l�mite
+   *
+   * @param direc char que indica hacia donde gira la moto
+   */
+  public void girar(char direc) {
+
+    if (direc == 'i') {
+      if (this.direccion > -2) {
+        this.direccion--;
+      } else if (direc == 'd') {
+        if (this.direccion < 2) {
+          this.direccion++;
+        }
+      }
+    }
+  }
+}
---
+++ MotoElectrica/test/examen/MotoElectricaTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package examen;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author nieto
+ */
+@RunWith(value = Parameterized.class)
+public class MotoElectricaTest {
+    
+    MotoElectrica moto = new MotoElectrica();
+    
+    
+    
+    @Test
+    public void testAcelerar() {
+    }
+
+    @Test
+    public void testFrenar() {
+    }
+
+    @Test
+    public void testGirar() {
+    }
+    
+}
